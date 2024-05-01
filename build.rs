cd1d456ea0e30feddf2643f971331252102ea194
Bayu <rizqi.bayu@ui.ac.id>
2024-05-02 00:07:16 +0700

    Payment service implementation.

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>>{
     tonic_build::configure()
         .build_server(true)
-        .compile(&["proto/services.proto"], &["proto"])?;
+        .compile(
+            &["proto/services.proto"],   
+            &["proto"],                 
+        )?;
     Ok(())
 }
